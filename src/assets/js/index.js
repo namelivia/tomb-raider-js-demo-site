031cb30d841591ae12ad77c334c90e4d86d980c3
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-04-24 21:01:11 +0200

    Dont load fontawesome from js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,1 @@
 import 'bootstrap';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
